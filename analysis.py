38a59ac3fc5bb6eab1541f64a7a2ca5793d3d099
yhongm <869429751@qq.com>
2018-09-13 15:10:47 +0800

    fix analysis

--- analysis.py
+++ analysis.py
@@ -1,20 +1,17 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import pandas as pd
 from os import path
 
+
 def parse_csv():
     dir = path.dirname('.')
-    print("dir:"+path.abspath(''))
+    print("dir:" + path.abspath(''))
 
-    df = pd.read_csv(path.join(dir, 'jobs.csv'))
-    r=df.groupby('classify_name')
-    v=r.classify_name.agg(['count'])
+    df = pd.read_csv(path.join(dir, 'jobs_all.csv'))
+    r = df.groupby('classify_name')
+    v = r.money.agg(['count', 'max', 'min'])
     print(v)
 
 
-
-
 if __name__ == "__main__":
-
     parse_csv()
-
